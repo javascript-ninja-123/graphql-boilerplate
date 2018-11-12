eae5a4602ed1806982983bcd1086c901abd255ec
Sung Yi <Sung.Yi@fox.com>
2018-11-12 09:36:19 -0800

    pushed

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { GraphQLServer } from 'graphql-yoga'
+// import prisma from './prisma';
+require('dotenv').config()
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+server.start(() => console.log('Server is running on localhost:4000'))
